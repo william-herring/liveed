8141381319a76ffe7849651c012d5ddb1fa85d1b
William Herring <73984595+william-herring@users.noreply.github.com>
2022-04-04 17:20:26 +1000

    begun navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Liveed</title>
+      </Head>
 
+      <nav>
+        <div className='hidden md:block'>
+          <div className='flex flex-row w-screen fixed p-4 shadow-lg items-center space-x-6 text-center'>
+            <a href='/' className='flex flex-row space-x-3 items-center'>
+              <Image src='/icon.png' width={36} height={36} /> <h1 className='font-bold text-md text-red-500'>Liveed</h1>
+            </a>
+          </div>
+        </div>
+        <div className='md:hidden'>
+          <div className='flex flex-row w-screen fixed p-4 shadow-lg items-center text-center'>
+            <a href='/'>
+              <Image src='/icon.png' width={36} height={36} />
+            </a>
+
+            <h1 className='font-bold text-lg text-red-500 flex-1'>Home</h1>
+
+            <button className='ml-auto mr-0'>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M21 18H3V16H21V18ZM21 13H3V11H21V13ZM21 8H3V6H21V8Z" fill="#ef4444" />
+                </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
