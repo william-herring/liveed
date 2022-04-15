1e4f71b4923ad82f9cefbf2e220b50ec46755fb1
William Herring <73984595+william-herring@users.noreply.github.com>
2022-04-16 08:48:23 +1000

    text editor layout

--- pages/feed/[id].tsx
+++ pages/feed/[id].tsx
@@ -6,6 +6,8 @@ import Header from "../../components/Header"
 import { useSession } from "next-auth/react"
 import Post from "../../components/Post"
 import { Feed } from "@prisma/client"
+import { useState } from "react"
+import TextEditor from "../../components/TextEditor"
 
 interface FeedProps extends Feed {
     author: { username: string }
@@ -35,6 +37,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 const FeedPage: NextPage<{ feed: FeedProps }> = (props) => {    
     const { data: session } = useSession()
+    const [editingPosts, setEditingPosts] = useState(0)
 
     return (
         <div>
@@ -63,17 +66,15 @@ const FeedPage: NextPage<{ feed: FeedProps }> = (props) => {
 
             <div className='flex flex-col items-center'>
                 <div className='w-5/6 md:w-1/2 space-y-6 bg'>
-                    {props.feed.posts.map((obj) => {
-                        return (
-                            <Post title={obj.title} content={obj.content} />
-                        )
-                    })}
+                    {[...Array(editingPosts)].map(() => <TextEditor />)}
+
+                    {props.feed.posts.map((obj) => <Post title={obj.title} content={obj.content} />)}
                 </div>
             </div>
             
             {session?.user?.name == props.feed.author.username? 
                 <div className='flex justify-end fixed bottom-0 w-screen p-6'>
-                    <button className='bg-red-500 p-3 rounded-full text-white font-bold hover:p-4'>
+                    <button className='bg-red-500 p-3 rounded-full text-white font-bold hover:bg-opacity-90' onClick={() => setEditingPosts(editingPosts + 1)}>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 10H17V7H14V5H17V2H19V5H22V7H19V10Z" fill="#ffff"></path>
                             <path d="M21 12H19V15H8.334C7.90107 14.9988 7.47964 15.1393 7.134 15.4L5 17V5H12V3H5C3.89543 3 3 3.89543 3 5V21L7.8 17.4C8.14582 17.1396 8.56713 16.9992 9 17H19C20.1046 17 21 16.1046 21 15V12Z" fill="#ffff"></path>
