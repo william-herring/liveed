257cb99b967285eacd89e88098da654002a14cce
William Herring <73984595+william-herring@users.noreply.github.com>
2022-04-21 12:42:34 +1000

    fixed z-ordering

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC<HeaderProps> = (props) => {
     const [showDrawer, setShowDrawer] = useState(false)
 
     return (
-        <nav className="fixed top-0 bg-white">
+        <nav className="fixed top-0 bg-white z-10">
           {showDrawer? <div className='flex flex-col w-screen h-screen bg-white'>
             <button onClick={() => setShowDrawer(false)} className='ml-auto'>
               <svg className='m-4' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
