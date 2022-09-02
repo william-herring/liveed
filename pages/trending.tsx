3a26274ba3d91c3eafd4c5331c9b12bde76d766d
William Herring <73984595+william-herring@users.noreply.github.com>
2022-09-02 18:08:46 +1000

    Removed create button on trending page

--- pages/trending.tsx
+++ pages/trending.tsx
@@ -74,15 +74,6 @@ const TrendingPage: NextPage<{ feeds: TrendingFeedProps[] }> = (props) => {
                                      author={obj.author.username} live={obj.live} />
                 })}
             </div>
-
-            {session? <div className='flex justify-end fixed bottom-0 w-screen p-6'>
-                <a className='bg-red-500 p-3 rounded-full hover:bg-opacity-90' href='/create'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M19 10H17V7H14V5H17V2H19V5H22V7H19V10Z" fill="#ffff"/>
-                        <path d="M21 12H19V15H8.334C7.90107 14.9988 7.47964 15.1393 7.134 15.4L5 17V5H12V3H5C3.89543 3 3 3.89543 3 5V21L7.8 17.4C8.14582 17.1396 8.56713 16.9992 9 17H19C20.1046 17 21 16.1046 21 15V12Z" fill="#ffff"/>
-                    </svg>
-                </a>
-            </div> : null}
         </div>
     )
 }
